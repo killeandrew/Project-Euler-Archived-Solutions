1916de603787a6bbab049947de59b3fc6538fe1d
kille <killeandrew@gmail.com>
2021-05-14 12:21:23 -0400

    update

--- Problem001.cpp
+++ Problem001.cpp
@@ -8,6 +8,19 @@
  
 using namespace std;
 
+void solve()
+{
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if ((i % 3 == 0) || (i % 5 == 0))
+            sum += i;
+    }
+    cout << sum;
+}
+
 int main()
 {
     int t;
