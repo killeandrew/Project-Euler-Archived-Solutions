1faf13f39dbd5501bda53d106f634ca3dfa1714d
Your Name <you@example.com>
2021-05-14 11:12:24 -0400

    update

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -1,3 +1,19 @@
 /*
     author: kille
 */
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+ 
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
