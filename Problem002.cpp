8bcce7e89ecf605a85e14a38ce1d2f11dcb6581c
kille <killeandrew@gmail.com>
2021-05-16 10:06:05 -0400

    not finished with this problem

--- Problem002.cpp
+++ Problem002.cpp
@@ -8,7 +8,22 @@
  
 using namespace std;
 
+ll fib(ll a)
+{
+    if (a <= 1)
+        return a;
+    return fib(a-1) + fib(a-2);
+}
+
 int main()
 {
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << fib(n);
+    }
+    return 0;
 }
